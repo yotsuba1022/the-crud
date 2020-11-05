9a67c21162f35eba46aa579d8e16f17f31e74116
clu <uruz73128@gmail.com>
2020-11-05 23:02:29 +0800

    Ticket Name: Update MySql startup settings. - Update my.cnf - Update schema init related script

--- mysql/init_sql_scripts/init_user.sql
+++ mysql/init_sql_scripts/init_user.sql
@@ -1,2 +1,2 @@
-CREATE USER IF NOT EXISTS 'clu'@'localhost' IDENTIFIED WITH mysql_native_password BY 'clu123';
+CREATE user IF NOT EXISTS 'clu'@'localhost' IDENTIFIED WITH mysql_native_password BY 'clu123';
 GRANT ALL PRIVILEGES ON the_crud.* TO 'clu'@'localhost';
